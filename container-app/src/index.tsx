a75f2b90f06db10816b1d1f22a48d7aa8274866b
Matt Billard <2836258+mattbillard@users.noreply.github.com>
2020-12-21 18:09:17 -0500

    Disable live reload

--- container-app/src/index.tsx
+++ container-app/src/index.tsx
@@ -6,3 +6,15 @@ import "./styles/styles.less";
 
 var mountNode = document.getElementById("container-app");
 ReactDOM.render(<ContainerApp />, mountNode);
+
+// CODE FROM: https://blog.iansinnott.com/how-to-disable-live-reload-in-create-react-app/
+// Disable live reload because it crashes proxy
+declare const window: any;
+const forceDisableLiveReload = () => {
+  console.info('Disabling live reload');
+
+  const WS = window.WebSocket;
+  const DevWebSocket =(s: any) => s.includes("/sockjs-node") ? {} : new WS(s);
+  window.WebSocket = DevWebSocket;
+}
+forceDisableLiveReload();
