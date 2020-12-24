ab04d1df0bc89846ffc033d85b9c181872fc5769
Matt Billard <2836258+mattbillard@users.noreply.github.com>
2020-12-24 17:39:48 -0500

    Copy Golden-Layout into container-app

--- container-app/src/components/container-app.tsx
+++ container-app/src/components/container-app.tsx
@@ -1,40 +1,49 @@
 import * as React from 'react';
 import { useState } from 'react';
+
+import { GoldenLayoutComponent } from './golden-layout-component/golden-layout-component';
 import { IframeComponent } from './iframe-component';
 import { WebComponent } from './web-component';
 
 interface IContainerAppProps {
 }
 
+// export const ContainerApp = (props: IContainerAppProps) => {
+//   const [count, setCount] = useState<number>(1);
+
+//   const url = '/micro-app/index.html';
+//   // const url = '/create-react-app';
+
+//   return (
+//     <div className="container">
+//       <h1>Container App</h1>
+//       <div>
+//         <button onClick={() => setCount(count - 1)}> - </button>
+//         <button onClick={() => setCount(count + 1)}> + </button>
+//       </div>
+
+//       {([...new Array(count)]).map((val, idx) => {
+//         return (
+//           // <div key={idx} className="">
+//           <div key={idx} className="row">
+//             <div>
+//               <h1>Iframe</h1>
+//               <IframeComponent url={url} />
+//             </div>
+//             <div>
+//               <h1>WebComponent</h1>
+//               <WebComponent url={url} />
+//             </div>
+//           </div>
+//         )
+//       })}
+//     </div>
+//   )
+// }
 export const ContainerApp = (props: IContainerAppProps) => {
-  const [count, setCount] = useState<number>(1);
-
-  const url = '/micro-app/index.html';
-  // const url = '/create-react-app';
-
   return (
     <div className="container">
-      <h1>Container App</h1>
-      <div>
-        <button onClick={() => setCount(count - 1)}> - </button>
-        <button onClick={() => setCount(count + 1)}> + </button>
-      </div>
-
-      {([...new Array(count)]).map((val, idx) => {
-        return (
-          // <div key={idx} className="">
-          <div key={idx} className="row">
-            <div>
-              <h1>Iframe</h1>
-              <IframeComponent url={url} />
-            </div>
-            <div>
-              <h1>WebComponent</h1>
-              <WebComponent url={url} />
-            </div>
-          </div>
-        )
-      })}
+      <GoldenLayoutComponent />
     </div>
   )
 }
