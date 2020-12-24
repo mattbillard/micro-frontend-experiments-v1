84c13e398771ede3125f8bbe2011578286aa49ba
Matt Billard <2836258+mattbillard@users.noreply.github.com>
2020-12-24 12:13:42 -0500

    Golden layout - clean up

--- golden-layout/src/assets/js/components/fibonacci-spiral.js
+++
@@ -1,7 +0,0 @@
-(function () {
-  myLayout.registerComponent('fibonacci-spiral', function (container, state) {
-    container
-      .getElement()
-      .append('<div class="fibonacci-spiral"></div>');
-  });
-})();
--- golden-layout/src/assets/js/components/react-component.js
+++
@@ -1,7 +0,0 @@
-var TestComponent = React.createClass({
-  render: function () {
-    return (<h1>{this.props.label}</h1>)
-  }
-});
-
-myLayout.registerComponent('testComponent', TestComponent);
