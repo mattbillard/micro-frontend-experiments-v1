40f127c36c6f71839191a5fc17436a3fc077b361
Matt Billard <2836258+mattbillard@users.noreply.github.com>
2021-01-09 21:57:48 -0500

    Iframes - try to not rerener onDrag

--- packages/container-app/src/components/iframe-component.tsx
+++ packages/container-app/src/components/iframe-component.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { memo } from 'react';
 // import { useRef, useState } from 'react';
 
 interface IIframeComponentProps {
@@ -6,35 +7,82 @@ interface IIframeComponentProps {
 }
 
 // TODO: clean up
+
+
+// export const IframeComponent = (props: IIframeComponentProps) => {
+//   // const ref = useRef(null);
+//   // const [height, setHeight] = useState<number>();
+
+//   // const setSize = (elem) => {
+//   //     setHeight(elem.offsetHeight);
+//   //     // setHeight(elem.scrollHeight);    
+//   // }
+
+//   // const handleOnload = () => {
+//   //   const iframe = ref.current;
+//   //   const iframeWindow = iframe.contentWindow;
+//   //   const iframeDocument = iframe.contentDocument;
+//   //   const iframeHtmlTag = iframeDocument.body.parentNode;
+
+//   //   const resizeObserver = new iframeWindow.ResizeObserver(entries => setSize(entries[0].target));
+//   //   resizeObserver.observe(iframeHtmlTag);
+
+//   //   // iframeWindow.addEventListener('resize', console.log);
+//   // }
+
+//   return (
+//     <iframe 
+//       // ref={ref} 
+//       src={props.url} 
+//       // onLoad={handleOnload}
+//       // style={{height:height}}
+//     ></iframe>
+//   )
+// }
+
+
+
+
 export const IframeComponent = (props: IIframeComponentProps) => {
-  // const ref = useRef(null);
-  // const [height, setHeight] = useState<number>();
-
-  // const setSize = (elem) => {
-  //     setHeight(elem.offsetHeight);
-  //     // setHeight(elem.scrollHeight);    
-  // }
-
-  // const handleOnload = () => {
-  //   const iframe = ref.current;
-  //   const iframeWindow = iframe.contentWindow;
-  //   const iframeDocument = iframe.contentDocument;
-  //   const iframeHtmlTag = iframeDocument.body.parentNode;
-
-  //   const resizeObserver = new iframeWindow.ResizeObserver(entries => setSize(entries[0].target));
-  //   resizeObserver.observe(iframeHtmlTag);
-
-  //   // iframeWindow.addEventListener('resize', console.log);
-  // }
-
   return (
-    <iframe 
-      // ref={ref} 
-      src={props.url} 
-      // onLoad={handleOnload}
-      // style={{height:height}}
-      style={{width:'100%',height:'100%'}} // TODO
-    ></iframe>
-  )
+    <iframe src={props.url}></iframe>
+  )  
 }
+
+
+// export const IframeComponent = memo((props: IIframeComponentProps) => {
+//   return (
+//     <iframe src={props.url}></iframe>
+//   )  
+// })
+
+
+
+
+// export class IframeComponent extends React.PureComponent {
+//   render () {
+//     return (
+//       <iframe src={props.url}></iframe>
+//     )  
+//   }
+// }
+
+
+// export class IframeComponent extends React.Component {
+//   shouldComponentUpdate(prevProps, nextProps) {
+//     console.log(prevProps?.url, nextProps?.url);
+    
+//     if (!nextProps) {
+//       return true;
+//     }
+
+//     return prevProps.url !== nextProps.url;
+//   }
+
+//   render () {
+//     return (
+//       <iframe src={props.url}></iframe>
+//     )  
+//   }
+// }
 
